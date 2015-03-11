e2c859ecfc49b7f4f2b1185849e2b008dd8f7d56
fanchangfa <fanchangfa@fanchangfa-Vostro-230.corp.ganji.com>
2015-03-11 19:07:23 +0800

    update file

--- Template.php
+++ Template.php
@@ -1,13 +1,16 @@
 <?php
+	/*模板类*/
 	class Template{
 		public $arrConfig = array(
 			'templeDir'		=>		'./templates/',		//模板目录
 			'cacheDir'		=>		'./templates_c',	//模板缓存目录
+			'compileDir'	=>		'./compiles',		//模板文件编译后存放目录
 			'profix'		=>		'.x',				//模板文件的扩展名
 			'cacheExpire'	=>		'10',				//缓存文件过期时间(单位:s)
+			'cache_html'	=>		true,				//是否需要生成静态文件
 		);
 
-		public $file = '';	//模板文件
+		public $file = '';	//模板文件,仅文件名，不包含路径和扩展
 
 		private static $compileTool;	//编译模板工具
 
@@ -23,35 +26,49 @@
 			}
 		}
 
+		/*
+			获取完整的模板文件路径
+		*/
 		public function path(){
-			return $this->arrConfig['templeDir'] . md5($this->file) . $this->arrConfig['profix'];
+			return $this->arrConfig['templeDir'] . $this->file . $this->arrConfig['profix'];
 		}
 
 		public function display($file){
 			$this->file = $file;
 
-			if($this->needCache()){
+			//当前模板文件对应的编译文件
+			$compileFile = $this->arrConfig['compileDir'] . md5($file) .'.php';
+
+			//当前模板文件对应的缓存静态文件
+			$cacheFile = $this->arrConfig['cacheDir'] . md5($file) .'.htm';
+			
+			if($this->needCache($cacheFile)){
 				//生成缓存文件
-				self::compileTool = new CompileClass($this->arrConfig , $this->path() , $this->file);
+				self::compileTool = new CompileClass($this->path() , $compileFile);
 			}
 		}
 
 		/*
-		是否需要缓存
+		是否需要重新生成静态文件
 		*/
-		public function needCache(){
-			if(is_file($this->path()) && filemtime($this->path()) < $this->arrConfig['cacheExpire']){
-				return false;
-			}else{
-				return true;
+		public function needCache($file){
+			if($this->arrConfig['cache_html'] !== false){
+				if(is_file($file) && 
+					(time() - @filemtime($file) < $this->arrConfig['cacheExpire'])){
+					return false;
+				}else{
+					return true;
+				}
 			}
+
+			return false;
 		}
 	}
 
 	class CompileClass{
-		private $distFile;
-		private $templateFile;
-		public function __construct($arrConfig , $distFile , $templateFile){
+		private $compileFile;		//编译后的文件
+		private $templateFile;		//待编译的文件
+		public function __construct($templateFile , $compileFile){
 			$this->distFile = $distFile;
 			$this->templateFile = $arrConfig['templeDir'] . $templateFile . $arrConfig['profix'];
 
