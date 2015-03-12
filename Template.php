39cc8c4df4dca5e570ed698d8f1d03dab6b5207a
fanchangfa <fanchangfa@fanchangfa-Vostro-230.corp.ganji.com>
2015-03-12 18:39:22 +0800

    compileFile modify

--- Template.php
+++ Template.php
@@ -5,9 +5,11 @@
 			'templeDir'		=>		'./templates/',		//模板目录
 			'cacheDir'		=>		'./templates_c',	//模板缓存目录
 			'compileDir'	=>		'./compiles',		//模板文件编译后存放目录
-			'profix'		=>		'.x',				//模板文件的扩展名
+			'profix'		=>		'.tpl',				//模板文件的扩展名
 			'cacheExpire'	=>		'10',				//缓存文件过期时间(单位:s)
+			'compileExpire'	=>		'10',				//编译文件多久重新生成一次(单位:s),关闭debug生效
 			'cache_html'	=>		true,				//是否需要生成静态文件
+			'debug'			=>		false,				//调试模式，开启后每次都重新编译模板
 		);
 
 		public $file = '';	//模板文件,仅文件名，不包含路径和扩展
@@ -38,14 +40,22 @@
 
 			//当前模板文件对应的编译文件
 			$compileFile = $this->arrConfig['compileDir'] . md5($file) .'.php';
-
 			//当前模板文件对应的缓存静态文件
 			$cacheFile = $this->arrConfig['cacheDir'] . md5($file) .'.htm';
-			
-			if($this->needCache($cacheFile)){
-				//生成缓存文件
+
+			$reCompileFile = $this->reCompile($compileFile); 
+			if($reCompileFile){
+				//生成模板编译文件
 				self::compileTool = new CompileClass($this->path() , $compileFile);
+			
+				//生成缓存文件
 			}
+
+			if( !$reCompileFile && $this->needCache($cacheFile)){
+				//生成缓存文件
+			}
+
+			
 		}
 
 		/*
@@ -62,6 +72,30 @@
 			}
 
 			return false;
+		}
+
+		/*
+		是否需要生成编译文件
+		*/
+		public function reCompile($compileFile){
+			if(!isset($this->arrConfig['debug'])){
+				$this->arrConfig['debug'] = false;
+			}
+
+			if($this->arrConfig['debug'] === true){
+				//调试模式
+				//文件超时
+				if(time()-@filemtime($compileFile) > $this->arrConfig['compileExpire'])){
+					return false;	
+				}
+				return true;
+			}else{
+				//若编译文件存在
+				if( @is_file($compileFile) ){
+					return false;
+				}
+				return true;
+			}
 		}
 	}
 
